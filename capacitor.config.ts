c6a6a2fa28976b05eb535d5d8df0fecffb81bb51
Jhon Helman <89268189+ajuly4003@users.noreply.github.com>
2024-02-13 14:24:27 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "cash.moneyflow.moneyflow",
-  appName: "Money Flow",
+  appId: "com.alquranweb.bn",
+  appName: "Cashier X",
   webDir: "dist",
   server: {
     url: "http://192.168.1.13:5173",
